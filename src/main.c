02344c8e38e143ea4d8c4e0eedae2568a126231d
Doctor-wonder <niallpeck@gmail.com>
2024-02-22 18:47:49 -0800

    Traffic light function creation

--- src/main.c
+++ src/main.c
@@ -30,14 +30,12 @@ static void GPIO_Setup( void );
 
 xQueueHandle xQueue_handle = 0;
 
-void led_init( void );
-void led_data( unsigned char data );
 void adc_initialize( void );
-void traffic_lights( int traffic_flow );
+void traffic_lights( float traffic_flow );
 void status_toggle( void );
 uint16_t adc_convert( void );
 
-void Delay( void );
+void delay( uint16_t delay_duration );
 
 unsigned char digits[10] = { 0x3f, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
@@ -55,7 +53,7 @@ int main(void)
 	prvSetupHardware();
 	GPIO_Setup();
 
-	//int traffic_flow = 2;
+	//float traffic_flow = 2;
 
 	// ADC Testing Function
 //	int count = 0;
@@ -72,6 +70,8 @@ int main(void)
 //
 //	/* Start the tasks and timer running. */
 //	vTaskStartScheduler();
+
+
 	uint16_t ADC_Value;
 	GPIOC->ODR |= GPIO_ODR_ODR_0;
 	GPIOC->ODR |= GPIO_ODR_ODR_1;
@@ -199,7 +199,19 @@ void adc_initialize( void )
 
 /*-----------------------------------------------------------*/
 
-void traffic_lights( int traffic_flow )
+/*
+Updates: Changed traffic_flow to a float as I might be needing decimals, but that loses precision?
+Working on finding algorithms to proportionally relate two values
+Yellow light needs to be consistent so that's easy
+Created a basic delay function for once the actual light durations are figured out
+
+TODO:
+- create proportional value algorithm
+- Test delay function and dial in the proper times
+- Test traffic_lights function with ADC and potentiometer
+*/
+
+void traffic_lights( float traffic_flow )
 {
 	GPIOC->ODR |= GPIO_ODR_ODR_0; // example of how to output to the LED
 	GPIOC->ODR |= GPIO_ODR_ODR_1;
@@ -209,22 +221,32 @@ void traffic_lights( int traffic_flow )
 	GPIOC->ODR |= GPIO_ODR_ODR_8;
 	GPIOC->ODR |= GPIO_ODR_ODR_9;
 
+	float min = 0;
+	float max = 3600;
+
+	float proportion_of_max = traffic_flow/max; // Am attempting to proportionally relate values
+
+	uint16_t red_duration = ; // This needs to be inversely proportional to traffic_flow
+	uint16_t yellow_duration = 4;
+	uint16_t green_duration = ;
 
 }
 
 /*-----------------------------------------------------------*/
 
-//void led_init( void )
-//{
-//
-//}
+/*
+This function is meant to create a delay of a specific amount. 
+I will need to test to see exactly how long of a delay this creates, not sure
+exactly how best to do that but the goal is to use this for the traffic light 
+function and others. Will test this independently in the lab when I have the chance.
+*/
 
-/*-----------------------------------------------------------*/
+void delay( uint16_t delay_duration )
+{
 
-//void led_data( unsigned char data )
-//{
-//
-//}
+	for( uint16_t i = 0; i < 4800000*delay_duration; i++ );
+
+}
 
 /*-----------------------------------------------------------*/
 
